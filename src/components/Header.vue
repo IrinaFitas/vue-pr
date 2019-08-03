26f7ac471fac9a52665a04442761b03f28a8a3b3
IrinaFitas <irinafitas07@gmail.com>
2019-08-03 13:12:45 +0300

    Created components using ElementUI library and added some style

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header-container">
+    <span>Dashboard</span>
+  </div>
+</template>
+
+<style scoped>
+  .header-container {
+    height: 60px;
+    padding: 20px;
+    box-sizing: border-box;
+    box-shadow: 0 0 3px grey;
+    font-weight: bold;
+  }
+</style>
+
